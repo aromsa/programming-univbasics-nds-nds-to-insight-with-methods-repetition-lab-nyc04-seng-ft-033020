15fbfffdf969baa85c552eaad0093b150ffc15a5
anna bateman <annacbateman@gmail.com>
2020-03-26 20:01:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,7 +29,7 @@ def list_of_directors(source)
   counter = 0 
   while counter < source.length do
     i = 0 
-    while i < directors_database.length
+    while i < source.length
     puts directors_database[source].count
     counter += 1 
 end
